55630bff8d1576db8b1e576afe832befb4042b43
PentestPython <julien.dev1609@outlook.com>
2024-02-25 19:47:29 +0100

    création collection & affichage sur page html

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.html import escape
+
+from tasks.models import Collection
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html', context={})
+    context = {}
+    context["collections"] = Collection.objects.order_by("name")
+    return render(request, 'tasks/index.html', context=context)
+
+def add_collection(request):
+    collection_name = escape(request.POST.get("collection-name"))
+    Collection.objects.create(name=collection_name)
+    return redirect('home')
