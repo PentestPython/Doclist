6c8d2550000c1cf53a079565f8f358d80e93fff3
PentestPython <julien.dev1609@outlook.com>
2024-02-26 04:19:11 +0100

    Supprimer une tâche

--- doclist/urls.py
+++ doclist/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
+
 import tasks.views as views
 
 urlpatterns = [
     path('', views.index, name="home"),
     path('add-collection/', views.add_collection, name="add-collection"),
     path('add-task/', views.add_task, name="add-task"),
+    path('delete-task/<int:task_pk>/', views.delete_task, name="delete-task"),
     path('get-tasks/<int:collection_pk>/', views.get_tasks, name="get-tasks"),
     path('admin/', admin.site.urls),
 ]
